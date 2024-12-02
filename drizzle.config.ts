bd1dcc2de5498db448a65bf6c9f95ce73f0c20c5
PXR <pxr2709@gmail.com>
2024-12-02 14:13:20 +0700

    cloudflare deploy config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,15 @@ export default defineConfig({
   dialect: "sqlite",
   schema: "./src/database/schema.ts",
   out: "./drizzle",
+  verbose: true,
+  strict: true,
   dbCredentials: {
     url: "mono.db",
+    // For Cloudflare D1
+    // accountId: process.env.ACCOUNT_ID!,
+    // databaseId: process.env.DB_ID!,
+    // token: process.env.CF_TOKEN!,
   },
+  // For Cloudflare D1
+  // driver: "d1-http",
 });
